2d148be3f5d4080e447eecfb84a7e1d69a12065a
Aguru Itabashi <itabashi.a@gmail.com>
2025-01-04 22:42:39 +0900

    DB設定完了

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getDatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // .env を読み込む
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        getDatabaseConfig(configService),
+    }),
+    // TypeOrmModule.forFeature([User]), // エンティティを登録
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
